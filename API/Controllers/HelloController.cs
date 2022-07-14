3f041cb9e5deaafbd3c5de55c1a63835a9a86c19
Jai <Jai@Jais-Air-2.bbrouter>
2022-07-14 17:33:38 +0530

    Controller added

---
+++ API/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        // GET: /<controller>/
+        public IActionResult Get()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
+
