fae0f314a1262386512b8729d42ef6b71358a708
Jai <Jai@Jais-Air-2.bbrouter>
2022-07-14 17:56:27 +0530

    Post req

--- API/Controllers/HelloController.cs
+++ API/Controllers/HelloController.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-
+using Newtonsoft.Json.Linq;
 
 namespace API.Controllers
 {
@@ -13,10 +12,21 @@ namespace API.Controllers
     public class HelloController : ControllerBase
     {
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Get()
         {
             return Ok("Hello world");
         }
+
+        [HttpPost]
+
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }
+
+
+
     }
 }
 
